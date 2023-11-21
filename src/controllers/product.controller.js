ffde42cf0d339637129535e7f10ecdcc28ace46d
Gauravtripathi12345 <tripathigaurav38@gmail.com>
2023-11-21 17:00:32 +0530

    Updating Controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,8 +1,13 @@
 
 import path from 'path';
-export default class ProductController {
+import ProductModel from '../models/product.model.js';
 
+export default class ProductController {
     getProducts(req, res) {
+        let products = ProductModel.get();
+        console.log(products);
         return res.sendFile(path.join(path.resolve(), "src", "views", "products.html"))
     }
 }
+
+
