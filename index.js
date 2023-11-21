88ea26c255c3043169f775396bfba124c097edc8
Gauravtripathi12345 <tripathigaurav38@gmail.com>
2023-11-21 16:25:02 +0530

    Creating Controller

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
+import ProductController from './src/controllers/product.controller.js';
 
 const server = express();
 
-server.get('/', (req, res) => {
-    return res.send("Welcome to Inventory App");
-});
+// create an instance of ProductController
+const productController = new ProductController();
 
-server.listen(3400);
+server.use(express.static('src/views'));
+server.get('/', productController.getProducts);
+// server.get('/', (req, res) => {
+//     return res.send("Welcome to Inventory App");
+// });
+
+
+server.listen(3400, () => {
+    console.log("Server is running at 3400");
+});
