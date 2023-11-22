94df5dab513c4c07c05dac3b1981eff985f85844
Gauravtripathi12345 <tripathigaurav38@gmail.com>
2023-11-22 11:52:18 +0530

    Creating express-ejs-layouts

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 import express from 'express';
 import path from 'path';
 import ProductController from './src/controllers/product.controller.js';
-
+import ejsLayouts from 'express-ejs-layouts';
 const server = express();
 
 server.set("view engine", "ejs");
 server.set("views", path.join(path.resolve(), "src", "views"));
 
+server.use(ejsLayouts);
 // create an instance of ProductController
 const productController = new ProductController();
 
